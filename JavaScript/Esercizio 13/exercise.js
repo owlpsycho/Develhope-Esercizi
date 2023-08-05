b51a3d98557ca996a95e156b48c48f8fc4b069f9
Denis Bruno <denisbruno.DB28@gmail.com>
2023-08-05 14:36:34 +0200

    esercizio 13

--- JavaScript/Esercizio 13/exercise.js
+++ JavaScript/Esercizio 13/exercise.js
@@ -1,5 +1,7 @@
 function calculateAverageAge(persons) {
-  // ...
+  const totalAge = persons.reduce((sum, person) => sum + person.age, 0);
+  const average = totalAge / persons.length;
+  return average;
 }
 
 const persons = [
