6b4ddb69881192c844296eabb18cc5d658f2cce0
Denis Bruno <denisbruno.DB28@gmail.com>
2023-08-24 17:36:29 +0200

    risoluzione esercizio 16

--- JavaScript/Esercizio 16/exercise.js
+++ JavaScript/Esercizio 16/exercise.js
@@ -1,4 +1,12 @@
 // Class definition
 
-const developer = new Person('Mario', 'Rossi');
+class Person {
+    constructor (firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+
+const developer = new Person("Mario", "Rossi");
 console.log(developer.firstName + " " + developer.lastName);
