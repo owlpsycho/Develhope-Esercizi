80e838f489a759d8ada62452a4e71462287569e1
Denis Bruno <denisbruno.DB28@gmail.com>
2023-08-25 12:59:17 +0200

    risoluzione esercizio 19

--- JavaScript/Esercizio 19/exercise.js
+++ JavaScript/Esercizio 19/exercise.js
@@ -1,5 +1,34 @@
 class Person {
-  // ...
+  constructor(firstName, lastName, age){
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._age = age;
+  }
+
+  get firstName(){
+    return this._firstName;
+  }
+  set firstName(newFirstName){
+    this._firstName = newFirstName;
+  }
+
+  get lastName(){
+    return this._lastName;
+  }
+  set lastName(newLastName){
+    this._lastName = newLastName;
+  }
+
+  get age(){
+    return this._age;
+  }
+  set age(newAge){
+    this._age = newAge;
+  }
+
+  get fullName(){
+    return this._firstName + ' ' + this._lastName;
+  }
 }
 
 const person = new Person('Mario', 'Rossi', 25);
