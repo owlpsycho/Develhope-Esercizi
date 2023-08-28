afe588f9ee925b9c1994dc7ac7e2a503c98eee98
Denis Bruno <denisbruno.DB28@gmail.com>
2023-08-28 13:47:21 +0200

    risoluzione esercizio 21

--- JavaScript/Esercizio 21/exercise.js
+++ JavaScript/Esercizio 21/exercise.js
@@ -1,10 +1,8 @@
 let num1 = 10;
 let num2 = 20;
-console.log('Before swap: ', num1, num2); // Before swap: 10 20
+console.log('Before destructuring: ', num1, num2); // Before: 10 20
 
-// Variable swapping
-let temp = num2;
-num2 = num1;
-num1 = temp;
+// Destructuring
+[num1, num2] = [num2, num1];
 
-console.log('After swap: ', num1, num2); // After swap: 20 10
+console.log('After destructuring: ', num1, num2); // After: 20 10
