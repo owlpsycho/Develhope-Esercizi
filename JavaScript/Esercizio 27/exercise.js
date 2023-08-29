f7b62cb415a9883fdd56654b857a93ce325b7af8
Denis Bruno <denisbruno.DB28@gmail.com>
2023-08-29 17:54:01 +0200

    risoluzione esercizio 27

--- JavaScript/Esercizio 27/exercise.js
+++ JavaScript/Esercizio 27/exercise.js
@@ -5,11 +5,13 @@ const person = {
   age: 25
 };
 
-const id = person.id;
-const personInfo = {
-  firstName: person.firstName,
-  lastName: person.lastName,
-  age: person.age
-};
+// const id = person.id;
+// const personInfo = {
+//   firstName: person.firstName,
+//   lastName: person.lastName,
+//   age: person.age
+// };
+
+const {id, ...personInfo} = person;
 
 console.log(id, personInfo);
