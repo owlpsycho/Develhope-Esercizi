7f76fc0987ac4db5f40c1834b70934a7d9aee422
Denis Bruno <denisbruno.DB28@gmail.com>
2023-08-29 15:10:03 +0200

    risoluzione esercizio 25

--- JavaScript/Esercizio 25/exercise.js
+++ JavaScript/Esercizio 25/exercise.js
@@ -1,4 +1,18 @@
 let numberStore = [0, 1, 2];
 let newNumber = 3;
-numberStore.push(newNumber);
-console.log(numberStore);
+
+numberStore = [...numberStore, newNumber]
+console.log(numberStore)
+
+// il metodo sopra mi sembra si addica di più alla consegna visto che 
+// chiede un merge quidno ho sovrascritto l'array principale, 
+
+
+
+//sotto invece creo un nuovo array con quelli precedenti
+
+// let numberStore = [0, 1, 2];
+// let newNumber = 3;
+
+// let newNumberStore = [...numberStore, newNumber]
+// console.log(newNumberStore)
