4d89842085206f3cde1e89bac6db4f7244ea43b1
Denis Bruno <denisbruno.DB28@gmail.com>
2023-08-29 17:58:31 +0200

    risoluzione esercizio 28

--- JavaScript/Esercizio 28/exercise.js
+++ JavaScript/Esercizio 28/exercise.js
@@ -1,6 +1,6 @@
-function sum(num1, num2, num3) {
-  return num1 + num2 + num3;
+function sum(...numbers) {
+  return numbers.reduce((a,b) => a + b);
 }
 
 const numbers = [1, 2, 3];
-console.log(sum(numbers[0], numbers[1], numbers[2]));
+console.log(sum(...numbers));
