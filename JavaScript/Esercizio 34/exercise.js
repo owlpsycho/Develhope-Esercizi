7b765781654c1c2b0e0e365b046c2d6aa3918a0d
Denis Bruno <denisbruno.DB28@gmail.com>
2023-09-05 11:55:13 +0200

    risoluzione esercizio 34

--- JavaScript/Esercizio 34/exercise.js
+++ JavaScript/Esercizio 34/exercise.js
@@ -1,1 +1,16 @@
+function PrintAsyncName(callback, name) {
+  const interval = setInterval(() => {
+    callback();
+    clearInterval(interval);
+  }, 1000);
 
+  setTimeout(() => {
+    console.log(name);
+  }, 2000);
+}
+
+function callback() {
+  console.log("Hello");
+}
+
+PrintAsyncName(callback, "Nino");
