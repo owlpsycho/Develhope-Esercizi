f9a7299242ee7e9bcb0985a62683100caeb9883c
Denis Bruno <denisbruno.DB28@gmail.com>
2023-09-05 12:48:05 +0200

    risoluzione esercizio 35

--- JavaScript/Esercizio 35/exercise.js
+++ JavaScript/Esercizio 35/exercise.js
@@ -1,1 +1,9 @@
+function repeatHello(callback) {
+setInterval (callback, 1000)
+}
 
+const sayHello = () => console.log("Hello");
+//The callback function must be an arrow function, can you also explain why?
+//Perché è più leggibile e il contesto this lo eredita dal genitore, senza crearlo autonomamente come le function normali
+
+repeatHello(sayHello)
