bed42253150fc983e20dd5521b49765b9a5ae12b
Denis Bruno <denisbruno.DB28@gmail.com>
2023-09-06 11:37:43 +0200

    risoluzione esercizio 37

--- JavaScript/Esercizio 37/exercise.js
+++ JavaScript/Esercizio 37/exercise.js
@@ -1,1 +1,12 @@
 const number = 15;
+let checkNumber = new Promise ((resolve, reject) => {
+    if (number > 10){
+        resolve (number)
+    }else {
+        reject ("Error: The Number isn't greater than 10")
+    }
+})
+
+checkNumber
+    .then ((val) => console.log(val))
+    .catch ((err) => console.error(err))
