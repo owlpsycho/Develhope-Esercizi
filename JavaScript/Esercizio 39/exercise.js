1994d8fbed90178f443b7628750be63faf4141d8
Denis Bruno <denisbruno.DB28@gmail.com>
2023-09-06 17:16:44 +0200

    risoluzione esercizio 39

--- JavaScript/Esercizio 39/exercise.js
+++ JavaScript/Esercizio 39/exercise.js
@@ -1,1 +1,31 @@
+const isLogged = true;
 
+const logCheeck = (isLogged) => {
+  return new Promise((resolve, reject) => {
+    if (isLogged) {
+      const randomNum = Math.random()
+      resolve(randomNum)
+    } else {
+      reject (new Error ("User isn't logged"))
+    }
+  })
+}
+
+const getUserInfo = (inputNumber) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (inputNumber > 0.5) {
+        const user = { name: "John", age: 24 }
+        resolve(user)
+      } else {
+        reject(new Error ("Number isn't greater than 0.5"))
+      }
+    }, 1000)
+  })
+}
+
+logCheeck(isLogged)
+  .then((randomNum) => getUserInfo(randomNum))
+  .then((user) => console.log(user))
+  .catch((err) => console.error(err))
+  .finally(() => console.log("FINE DELL'ESERCIZIO"))
