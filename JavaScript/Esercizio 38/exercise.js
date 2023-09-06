3ff0f4f2397a8a647565ca8ef2e0001d9b34d9cb
Denis Bruno <denisbruno.DB28@gmail.com>
2023-09-06 16:04:08 +0200

    risoluzione esercizio 38

--- JavaScript/Esercizio 38/exercise.js
+++ JavaScript/Esercizio 38/exercise.js
@@ -1,1 +1,30 @@
-const isLogged = true;
+const isLogged = false;
+
+const logCheeck = (isLogged) => {
+  return new Promise((resolve, reject) => {
+    if (isLogged === true) {
+      const randomNum = Math.random()
+      resolve(randomNum)
+    } else {
+      reject("User isn't logged")
+    }
+  })
+}
+
+const secondPromise = (inputNumber) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (inputNumber > 0.5) {
+        const user = { name: "John", age: 24 }
+        resolve(user)
+      } else {
+        reject("Number isn't greater than 0.5")
+      }
+    }, 1000)
+  })
+}
+
+logCheeck(isLogged)
+  .then((randomNum) => {return secondPromise(randomNum)})
+  .then((user) => console.log(user))
+  .catch((err) => console.error(err))
