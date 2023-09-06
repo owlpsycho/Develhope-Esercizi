92bf197cc4d82c7654f9f01976619b54d9ea9457
Denis Bruno <denisbruno.DB28@gmail.com>
2023-09-06 16:05:42 +0200

    risoluzione esercizio 35

--- JavaScript/Esercizio 35/exercise.js
+++ JavaScript/Esercizio 35/exercise.js
@@ -1,8 +1,7 @@
 function repeatHello(callback) {
 setInterval (()=>{
     callback()
-}, 1000)
-}
+}, 1000)}
 
 const sayHello = () => console.log("Hello");
 //The callback function must be an arrow function, can you also explain why?
