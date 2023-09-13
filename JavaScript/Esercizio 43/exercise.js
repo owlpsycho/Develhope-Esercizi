73abb84519d1427fffa7c36370f6af388a708d8f
Denis Bruno <denisbruno.DB28@gmail.com>
2023-09-13 12:50:26 +0200

    risoluzione esercizio 43

--- JavaScript/Esercizio 43/exercise.js
+++ JavaScript/Esercizio 43/exercise.js
@@ -3,3 +3,15 @@ const user = {
   name: "John",
   age: 25,
 };
+
+function saveUser(user) {
+  try {
+    const userJSON = JSON.stringify(user);
+    localStorage.setItem('user', userJSON);
+    console.log('User saved to localStorage.');
+  } catch (error) {
+    console.error('Error saving user to localStorage:', error);
+  }
+}
+
+saveUser(user);
