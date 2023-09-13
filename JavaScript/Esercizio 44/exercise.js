2b3d8a94bfd7f1c8112c57530ebc25eaad8597ea
Denis Bruno <denisbruno.DB28@gmail.com>
2023-09-13 17:36:55 +0200

    risoluzione esercizio 44

--- JavaScript/Esercizio 44/exercise.js
+++ JavaScript/Esercizio 44/exercise.js
@@ -3,3 +3,14 @@ const user = {
   name: "John",
   age: 25,
 };
+
+function getUser(data) {
+  try {
+      const savedUser = JSON.parse(localStorage.getItem(data));
+      console.log("Utente recuperato da LocalStorage:", savedUser);
+  } catch (error) {
+      console.error(error);
+  }
+}
+
+getUser("user");
